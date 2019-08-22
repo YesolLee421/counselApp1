5e863d9ef696ea0b75641d000148e87741505e60
Yesola <dldPthf421@gmail.com>
2019-08-22 21:52:15 +0900

    imageView의 drawble 자원 그리기에 문제 생긴 이유는 너무 낮은 버전 기기를 사용해서였다.

---
+++ app/src/main/java/com/example/counselapp/LogInActivity.kt
@@ -0,0 +1,16 @@
+package com.example.counselapp
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
+class LogInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_login)
+
+
+    }
+
+
+}
--- app/src/main/java/com/example/counselapp/MainActivity.kt
+++ app/src/main/java/com/example/counselapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.counselapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,7 +8,13 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
+
+//    override fun onResume() {
+//        super.onResume()
+//        val intent = Intent(this, LogInActivity::class.java)
+//        startActivity(intent)
+//    }
 
 }
